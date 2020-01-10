5a808170885f122bc199ce47dbebe2475d705553
Ignacio Andrés Codina <igcodinap@gmail.com>
2020-01-10 15:37:19 -0300

    Plus events capture

--- main.py
+++ main.py
@@ -2,16 +2,55 @@ import json
 import facebook
 
 def main():
-    token = {"EAAOpVL6foG8BAAcZASxOCCZAE6qjHbNUPFmciLyAGCFRWhhq8XCEGFNIybiyuHatRlZAwpEcitC8EyhNjPt8h37dphwW0yj8ZC0hcZCIeRnfte8HLgUS7SjvheYINenvpdXYlsrkR8bMCYV4D7hWOMXGQwWD2NLWEAB8GiHSCuZAp8lntt3cr3Mgbr6LpBHW15jnyxPFnI8eNLsNgjABMkZAuqIqtu0T3JBSto5aherHQZDZD"}
+    token = {"EAALShho8E80BAN9rSjWcHCdIuffH9p4nFi7gMJBTF0o8i7FpZCvdjhZAhlXtxJzy9vmIwmB1HfVxAvTyteEkVRDtUhvupOd3TnS8xNjDoiFhsGZC9RJvVgI5RXqQ8zYJ2cUFJmrWzVlgFfILETEGgSwKznsFj1ZAqZAR4IeG653k7SBaAdIcNPkWVhDOSzLEl378hVMQfLtBkl3tJMaNaV8YTjce6NlwUQVEj1kLtmAZDZD"}
     graph = facebook.GraphAPI(token)
 
     fields = ['name,email, events']
 
-    ids = [10156853388023027, 10221292185153045]
+    ids = [10220832650024954]
     
+    def exists(var):
+        if var:
+            return var
+        else:
+            return ""
+
     for id in ids:
         profile = graph.get_object(id, fields = fields)
-        print(json.dumps(profile, indent=4))
+        """print(json.dumps(profile, indent=4))"""
+        data = json.dumps(profile, indent=4)
+        print(type(data))
+        decoded_data = json.loads(data)
+        """print(type(decoded_data))"""
+        events = decoded_data["events"]["data"]
+        eventsqty = len(events)
+        readingevent = 0
+        while readingevent < eventsqty:
+            """print(decoded_data["events"]["data"][readingevent])"""
+            event_id = exists(decoded_data["events"]["data"][readingevent].get("id"))
+            event_name = exists(decoded_data["events"]["data"][readingevent].get("name"))
+            description = exists(decoded_data["events"]["data"][readingevent].get("description"))
+            city = exists(decoded_data["events"]["data"][readingevent]["place"]["location"].get("city"))
+            street = exists(decoded_data["events"]["data"][readingevent]["place"]["location"].get("street"))
+            lat = exists(decoded_data["events"]["data"][readingevent]["place"]["location"].get("latitude"))
+            longi = exists(decoded_data["events"]["data"][readingevent]["place"]["location"].get("longitude"))
+            start_time = exists(decoded_data["events"]["data"][readingevent].get("start_time"))
+            end_time = exists(decoded_data["events"]["data"][readingevent].get("end_time"))
+
+            print(event_name)
+            print(event_id)
+            print(description)
+            print(city)
+            print(street)
+            print(lat)
+            print(longi)
+            print(start_time)
+            print(end_time)
+            readingevent +=1
+            print("*********************")
+
+
+        
 
 if __name__ =="__main__":
     main()
